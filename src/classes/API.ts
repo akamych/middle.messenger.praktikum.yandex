5558b4bd9c086e426d67a5ec7c7d1f698ed94282
Alexey "akamych" Kalinin <akamych@gmail.com>
2023-11-15 21:09:05 +0100

    api requests class

--- src/classes/API.ts
+++ src/classes/API.ts
@@ -1,78 +1,82 @@
+type ApiOptionsType = {
+  method: string;
+  headers?: Record<string, string>;
+  // eslint-disable-next-line no-undef
+  data?: Document | XMLHttpRequestBodyInit;
+  timeout?: number;
+};
 
-class Requests {
+export default class API {
+  static METHODS : Record<string, string> = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    DELETE: 'DELETE',
+  };
 
-    static METHODS = {
-        GET: 'GET',
-        POST: 'POST',
-        PUT: 'PUT',
-        DELETE: 'DELETE',
-    };
+  _queryStringify(data: object) {
+    const keys : string[] = Object.keys(data);
+    // eslint-disable-next-line max-len
+    return keys.reduce((result, key, index) => `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`, '?');
+  }
 
-    getUrlMaker(data) {
-        if (typeof data !== 'object') {
-            throw new Error('Data must be object');
-        }
-        const keys = Object.keys(data);
-        return keys.reduce((result, key, index) => {
-        return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
-        }, '?');
-    };
-    
-    get = (url, options = {}) => {
-             
-            return this.request(url, {...options, method: METHODS.GET}, options.timeout);
-    };
+  _rewriteMethod(method: string, options?: ApiOptionsType) : ApiOptionsType {
+    return { ...options, method };
+  }
 
-    post = (url, options = {}) => {
-            return this.request(url, {...options, method: METHODS.POST}, options.timeout);
-    };
+  get = (url: string, options?: ApiOptionsType) => {
+    let fullUrl = url;
+    if (options && options.data) {
+      fullUrl += this._queryStringify(options.data as object);
+    }
+    this._request(fullUrl, this._rewriteMethod(API.METHODS.GET, options));
+  };
 
-    put = (url, options = {}) => {
-            return this.request(url, {...options, method: METHODS.PUT}, options.timeout);
-    };
+  post = (url: string, options?: ApiOptionsType) => {
+    this._request(url, this._rewriteMethod(API.METHODS.POST, options));
+  };
 
-    delete = (url, options = {}) => { 
-            return this.request(url, {...options, method: METHODS.DELETE}, options.timeout);
-    };
+  put = (url: string, options?: ApiOptionsType) => {
+    this._request(url, this._rewriteMethod(API.METHODS.PUT, options));
+  };
 
-    request = (url, options = {}, timeout = 5000) => {
-            const {headers = {}, method, data} = options;
+  delete = (url: string, options?: ApiOptionsType) => {
+    this._request(url, this._rewriteMethod(API.METHODS.DELETE, options));
+  };
 
-            return new Promise(function(resolve, reject) {
-                    if (!method) {
-                            reject('No method');
-                            return;
-                    }
+  _request = (url: string, options? : ApiOptionsType) => {
+    const {
+      headers = {},
+      method,
+      data,
+      timeout = 5000,
+    } = options;
 
-                const xhr = new XMLHttpRequest();
-                    const isGet = method === METHODS.GET;
+    return new Promise((resolve, reject) => {
+      if (!method) {
+        reject(new Error('Не указан метод'));
+        return;
+      }
 
-                xhr.open(
-                            method, 
-                            isGet && !!data
-                                    ? `${url}${queryStringify(data)}`
-                                    : url,
-                    );
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.timeout = timeout;
 
-                    Object.keys(headers).forEach(key => {
-                            xhr.setRequestHeader(key, headers[key]);
-                    });
-            
-                xhr.onload = function() {
-                      resolve(xhr);
-                };
-            
-                xhr.onabort = reject;
-                xhr.onerror = reject;
-            
-                xhr.timeout = timeout;
-                xhr.ontimeout = reject;
-                    
-                  if (isGet || !data) {
-                        xhr.send();
-                    } else {
-                            xhr.send(data);
-                    }
-          });
-    };
+      Object.keys(headers).forEach((key : string) => {
+        xhr.setRequestHeader(key, headers[key]);
+      });
+
+      xhr.onload = () => resolve(xhr);
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.ontimeout = reject;
+
+      if (method === API.METHODS.GET || !data) {
+        xhr.send();
+      } else {
+        xhr.send(data);
+      }
+    });
+  };
 }
